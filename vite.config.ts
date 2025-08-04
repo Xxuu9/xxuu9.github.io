f73ea9e9b848e3f529a5d5ceccb1f72db997724c
XX <77370174+Xxuu9@users.noreply.github.com>
2025-08-04 13:44:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: '/xxuu9.github.io/'
   plugins: [react(), tailwindcss()],
 });
